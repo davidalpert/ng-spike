684b9ef8cc581c1c2533cb894cd05c3e9ef82f6a
David Alpert <david@spinthemoose.com>
2014-04-08 13:35:09 -0500

    adding a locally-hosted web server

--- HockeyStats.CLI/Program.cs
+++ HockeyStats.CLI/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.Diagnostics;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace HockeyStats.CLI
 {
@@ -10,6 +13,22 @@ namespace HockeyStats.CLI
     {
         static void Main(string[] args)
         {
+            string uri = "http://localhost:8080/";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
         }
     }
 }
