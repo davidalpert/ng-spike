375aff13d983114aa2ca9bceccb36f8ba9f46799
David Alpert <david@spinthemoose.com>
2014-04-08 15:48:07 -0500

    simplifying

--- HockeyStats.CLI/Program.cs
+++ HockeyStats.CLI/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Owin.Diagnostics;
 using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.Hosting;
-using Microsoft.Owin.StaticFiles;
 using Owin;
 
 namespace HockeyStats.CLI
@@ -30,21 +24,12 @@ namespace HockeyStats.CLI
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureStaticFiles(app);
-
-            app.UseWelcomePage();
-        }
-
-        private static void ConfigureStaticFiles(IAppBuilder app)
-        {
             app.UseFileServer(o =>
             {
                 o.FileSystem = new PhysicalFileSystem(".\\www");
-                o.EnableDefaultFiles = true;
-#if DEBUG
-                o.EnableDirectoryBrowsing = true;
-#endif
             });
+
+            app.UseWelcomePage();
         }
     }
 }
